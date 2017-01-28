80af4f1ec208d08360735e5afae202accd95a56a
isa <isaliangyiyun@hotmail.com>
2017-01-28 11:11:42 -0500

    add earth texture

--- js/main.js
+++ js/main.js
@@ -6,19 +6,25 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.SphereGeometry( 0.5, 32, 32 );
-var material = new THREE.MeshBasicMaterial();
+var geometry = new THREE.SphereGeometry( 2.0, 64, 64 );
 
-var loader=new THREE.TextureLoader();
-loader.setCrossOrigin("anonymous");
-loader.load(
-  'images/earthmap.jpg',
-  function (texture) {
-		var material = new THREE.MeshBasicMaterial({
-			map: texture
-		});
-	}
-);
+var loader = new THREE.TextureLoader();
+var colorMap = loader.load('../images/earthmap.jpg');
+var specMap = loader.load('../images/earthspec.jpg');
+var normalMap = loader.load('../images/earthbump.jpg');
+colorMap.minFilter = THREE.LinearFilter;
+specMap.minFilter = THREE.LinearFilter;
+normalMap.minFilter = THREE.LinearFilter;
+
+var material = new THREE.MeshBasicMaterial({
+  color: 0xffffff,
+  specular: 0x333333,
+  shininess: 15,
+  side: THREE.DoubleSide,
+  map: colorMap,
+  specularMap: specMap,
+  normalMap: normalMap
+});
 
 var earth = new THREE.Mesh( geometry, material );
 scene.add( earth );
@@ -28,7 +34,6 @@ camera.position.z = 5;
 var render = function () {
   requestAnimationFrame( render );
 
-  earth.rotation.x += 0.01;
   earth.rotation.y += 0.01;
 
   renderer.render(scene, camera);
